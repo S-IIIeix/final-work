762653c91c846a2b33d82ed575862d9a8516470c
S-IIIeix <petrov.m.s.mobilka@gmail.com>
2024-12-18 01:30:03 +0300

    first

---
+++ Quiz/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QuizApp
+{
+    public class Question
+    {
+        public string Text { get; }
+        public string Answer { get; }
+
+        public Question(string text, string answer)
+        {
+            Text = text;
+            Answer = answer;
+        }
+    }
+}
---
+++ Quiz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizApp
+{
+    class QuizProgram
+    {
+        static void Main()
+        {
+            var quiz = new List<Question>
+            {
+                new Question("Какой океан самый глубокий?", "Тихий"),
+                new Question("Столица Франции?", "Париж"),
+                new Question("Какой элемент обозначается символом O?", "Кислород"),
+                new Question("Кто написал 'Войну и мир'?", "Толстой"),
+                new Question("Сколько дней в феврале в высокосный год?", "29")
+            };
+
+            int score = 0;
+            foreach (var question in quiz)
+            {
+                Console.WriteLine(question.Text);
+                string userAnswer = Console.ReadLine()?.Trim();
+
+                if (string.Equals(userAnswer, question.Answer, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Правильно!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Неправильно! Правильный ответ: {question.Answer}");
+                }
+            }
+
+            Console.WriteLine($"Ваш итоговый балл: {score} из {quiz.Count}");
+        }
+    }
+}
